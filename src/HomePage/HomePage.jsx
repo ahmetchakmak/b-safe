bd84286fecc5e350a2269ef1aa5e0075584816cf
Ahmet Cakmak <acakmak@panth.com>
2022-04-02 09:06:06 -0400

    B-Safe Initialization

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -17,8 +17,8 @@ class HomePage extends React.Component {
         const { user, users } = this.props;
         return (
             <div className="col-md-6 col-md-offset-3">
-                <h1>Hi {user.firstName}!</h1>
-                <p>You're logged in with React!!</p>
+                <h2>Hi {user.firstName}!</h2>
+                <p>You're logged in to B-Safe</p>
                 <h3>All registered users:</h3>
                 {users.loading && <em>Loading users...</em>}
                 {users.error && <span className="text-danger">ERROR: {users.error}</span>}
